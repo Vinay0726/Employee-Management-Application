d8eeb1d264476804dfcaa2f1a14e4dbb9fde25f5
Vinay0726 <vinaythaware143@gmail.com>
2024-08-13 15:19:20 +0530

    project started

---
+++ src/test/java/com/emservices/Employee/Management/Application/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emservices.Employee.Management.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
